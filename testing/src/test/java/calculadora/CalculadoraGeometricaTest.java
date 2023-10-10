8f7f50a9c789929fbe45a65a2cf564fb1485c3e7
Nacho Raik <ignacioraiksg@gmail.com>
2023-10-09 21:48:18 -0300

    add

--- testing/src/test/java/calculadora/CalculadoraGeometricaTest.java
+++ testing/src/test/java/calculadora/CalculadoraGeometricaTest.java
@@ -24,10 +24,26 @@ class CalculadoraGeometricaTest {
     }
 
     @Test
+    public void calculoTangenteTest() {
+        CalculadoraGeometrica calculadora = new CalculadoraGeometrica();
+        assertEquals( 1, calculadora.tan(45), 0.001);
+        assertEquals(Math.sqrt(3), calculadora.tan(60),  0.001);
+    }
+
+    @Test
     public void calculoHipotenusaTest() {
         CalculadoraGeometrica calculadora = new CalculadoraGeometrica();
-        assertEquals( 5, calculadora.hipotenusa(3, 4));
-        assertEquals(Math.sqrt(2), calculadora.hipotenusa(1, 1));
+        double opuesto = 1;
+        double adyacente = 1;
+        double angulo = 45;
+
+        double hip2 = adyacente / calculadora.cos(angulo); // raiz 2
+
+        assertEquals(5, calculadora.hipotenusa(3, 4));
+        assertEquals(hip2, calculadora.hipotenusa(opuesto, adyacente), 0.0000001);
     }
+
+
+
 
 }
