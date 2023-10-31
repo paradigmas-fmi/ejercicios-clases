1f0f168d9981efff444c394fdddfdf5c6e559951
Nacho Raik <ignacioraiksg@gmail.com>
2023-10-31 18:37:43 -0300

    Try without module-info

--- tatedrez/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.jline;
-
-    opens org.example.controller to javafx.fxml;
-    exports org.example;
-}
